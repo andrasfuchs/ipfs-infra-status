a481c8bedb628c35d64906779d61228921cdac1c
Andras Fuchs <andras.fuchs@gmail.com>
2019-08-14 18:01:20 +0200

    Update header HTML styles
    Update header HTML styles to be the same as explore.ipld.io.

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,11 +2,17 @@ import React from 'react'
 import ipfsLogo from '../../static/ipfs-logo.svg'
 
 const Header = () =>
-  <header className='flex items-center pa3 bg-navy'>
-    <a className='w-50' href='https://ipfs.io' target='_blank' title='ipfs.io'>
-      <img src={ipfsLogo} height='50' />
+  <header className='flex-l items-center pa3 bg-navy bb bw3 border-aqua tc tl-l'>
+    <a href='https://ipfs.io' title='IPFS' className='flex-none v-mid'>
+      <img src={ipfsLogo} alt='IPFS' style={{ height: 50, width: 117.5 }} />
     </a>
-    <h1 className='w-50 ma0 tr f3 fw2 montserrat aqua'>Public Gateways</h1>
+    <div className='flex-auto ph2 ph3-l pt1'>
+      <div style={{ maxWidth: 560 }} className='center-m'>
+      </div>
+    </div>
+    <h1 className='pt2 pt0-l ma0 f3 fw2 montserrat aqua ttu flex-none'>
+      {'Public Gateways'}
+    </h1>
   </header>
 
 export default Header
